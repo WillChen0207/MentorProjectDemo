a9d88fd49127a57587b7e526d13f765b2d4e0532
orange <1397057773@qq.com>
2022-06-20 17:09:20 +0800

    bootstrap test

--- src/main/java/com/mentorproject/HelloController.java
+++ src/main/java/com/mentorproject/HelloController.java
@@ -3,11 +3,12 @@ package com.mentorproject;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class HelloController {
-    @RequestMapping(value = {"/hello","/hi"}, method = RequestMethod.GET)
-    public String hello() {
-        return "Hello World!";
+    @RequestMapping(value = {"/","/homepage"}, method = RequestMethod.GET)
+    public ModelAndView index() {
+        return new ModelAndView("/homepage");
     }
 }
